8fea16f9e969300aa4c201946252a0186f5f54fa
Pedro <ppls2106@gmail.com>
2019-10-01 16:43:18 -0300

    born

---
+++ src/main/java/b/pa/gov/iec/bluwitter/BluwitterApplication.java
@@ -0,0 +1,13 @@
+package b.pa.gov.iec.bluwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluwitterApplication.class, args);
+	}
+
+}
