705aeb886c9fc1819bcfbe59250eb9dcabdf722f
Pedro Sousa <ppls2106@gmail.com>
2019-10-02 00:42:06 -0300

    ajuste inicial

---
+++ src/main/java/b/pa/gov/iec/bluwitter/controller/MensagemController.java
@@ -0,0 +1,10 @@
+package b.pa.gov.iec.bluwitter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mensagem")
+public class MensagemController {
+
+}
